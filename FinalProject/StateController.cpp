9837371801ffb27575c1f73b60c053c52c70368f
Albert liu <albertliu1992@hotmail.com>
2019-11-27 14:26:11 -0800

    Uncommented some issues with Protocol Thread

--- FinalProject/StateController.cpp
+++ FinalProject/StateController.cpp
@@ -21,48 +21,48 @@ void StateController::handleWrite() {
 	HANDLE dwEvent{ 0 };
 
 	while (comm->getIsComActive) {
-		if (WaitCommEvent(sessionService->readThread, &dwEvent, 0)) {
-			// If current state is RTR and reading thread receives a frame, send ACK or REQ
-			// inside the comm message, we will check if the current system has something to send
-			if (state == STATES::RTR) {
-				comm->sendCommunicationMessage(sessionService->getEvents().receivedFrame);
-			}
-			else if (state == STATES::RTS) {
-				// condition for empty write buffer to send EOT
-				
-				// condition for frame acknowledged by an ACK
-				// condition for frame acknowledged by a REQ
-				// condition for the first initial send
-				
-			}
+		//if (WaitCommEvent(sessionService->readThread, &dwEvent, 0)) {
+		//	// If current state is RTR and reading thread receives a frame, send ACK or REQ
+		//	// inside the comm message, we will check if the current system has something to send
+		//	if (state == STATES::RTR) {
+		//		comm->sendCommunicationMessage(sessionService->getEvents().receivedFrame);
+		//	}
+		//	else if (state == STATES::RTS) {
+		//		// condition for empty write buffer to send EOT
+		//		
+		//		// condition for frame acknowledged by an ACK
+		//		// condition for frame acknowledged by a REQ
+		//		// condition for the first initial send
+		//		
+		//	}
 
-		}
+		//}
 	}
 }
 
 
 
 
-//QUESTION Reset events everytime we receive new input? What happens if we have multiple events that are signalled and haven't been reset? How wwould we run into those scenarios?
+// QUESTION Reset events everytime we receive new input? What happens if we have multiple events that are signalled and haven't been reset? 
+// How wwould we run into those scenarios?
 void StateController::handleInput(char* input)
 {
+	// 
 	switch (state) {
 	case TX:
 		// Expect a REQ or ACK synch bit will be handled in statecontroller 2 bytes
-		
+
 		break;
 	case PREP_TX:
 		// Expect a ACK0 or ACK1 ?to get control of line Control Code Only 2 bytes
-		readHandle(CONTROL_CODE_LENGTH);
+		
 		break;
 	case IDLE:
 		//Expect a ENQ and only an ENQ Control Code only
-		readHandle(CONTROL_CODE_LENGTH);
 		break;
 	case RTR:
 		//Expect a data frame
 		//Or could be an EOT this is the only Staete that should handle either 1028 bytes or 2 byte response
-		readHandle(FRAME_LENGTH);
 		break;
 	//switch (state) {
 	//	/* Send States*/
